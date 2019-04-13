340d17b48299ef37bde99d5b3a49b9b29ccc38f5
cxan <c.p.xanten@gmail.com>
2019-04-13 21:45:46 +0200

    finished exercise 3 and added graphic for exercise 5

--- 02/peters-02.R
+++ 02/peters-02.R
@@ -11,13 +11,16 @@ regressionFunction <- function(x) {
 
 # a)
 plot(data$Verfügbares_Einkommen_pro_Einwohner, data$Wohnmobile_pro_1000_Einwohner,
-     xlab = 'Verfuegbares Jahreseinkommen (Euro)', ylab = 'Wohnmobile pro 1000 Einwohner',
+     xlab = 'Verfuegbares Jahreseinkommen (Euro)',
+     ylab = 'Wohnmobile pro 1000 Einwohner',
      main = 'Wohnmobile nach Einkommen')
 curve(regressionFunction, add=TRUE)
 
 # Bewertung:
-# Wie im Plot zu sehen, spiegelt die Gerade den Zusammenhang zwischen Jahreseinkommen und Wohnmobilanzahl recht angemessen wieder.
-# Ein anderer Zusammenhang als der durch die Gerade repraesentierte lineare Zusammenhang erscheint mir ueberdies wenig plausibel.
+# Wie im Plot zu sehen, spiegelt die Gerade den Zusammenhang zwischen
+# Jahreseinkommen und Wohnmobilanzahl recht angemessen wieder.
+# Ein anderer Zusammenhang als der durch die Gerade repraesentierte lineare
+# Zusammenhang erscheint mir ueberdies wenig plausibel.
 # Ich sehe daher keine belastbare Grundlage dazu, die Theorie von Schorsch anzufechten.
 
 # b)
@@ -25,16 +28,22 @@ residuals <- by(data, 1:nrow(data), function(row) {
   row$Wohnmobile_pro_1000_Einwohner - regressionFunction(row$Verfügbares_Einkommen_pro_Einwohner)
 })
 
-plot(data$Verfügbares_Einkommen_pro_Einwohner, residuals, xlab = 'Verfuegbares Jahreseinkommen (Euro)', ylab = 'Residuum', main = 'Residuen')
+plot(data$Verfügbares_Einkommen_pro_Einwohner, residuals,
+     xlab = 'Verfuegbares Jahreseinkommen (Euro)', ylab = 'Residuum',
+     main = 'Residuen')
+# There is no structure or pattern recognizable in the distribution of the residuals.
+# It seems that the model errors are randomly distributed around zero
+# which is an indicator that the linear model is an adequate fit in this situation.
 
 # c)
-# Get the total number of caravans for each place
-caravanCounts <- by(data, 1:nrow(data), function(row) {
-  row$Wohnmobile_pro_1000_Einwohner * row$Einwohner / 1000
+# Find out, where according to the model, the most caravans can be sold.
+# Places where the model's prediction is greater than the actual number of caravans
+# are considered to have higher potential than places where this number is lower.
+potentials <- by(data, 1:nrow(data), function(row) {
+  (regressionFunction(row$Verfügbares_Einkommen_pro_Einwohner) -
+     row$Wohnmobile_pro_1000_Einwohner) * row$Einwohner / 1000
 })
-# determine the maximum
-maxIndex <- which(caravanCounts == max(caravanCounts))
-city <- as.character(data[maxIndex, 'Landkreis_Stadt'])
-
+maxIndex <- which(potentials == max(potentials))
+city <- data[maxIndex, 'Landkreis_Stadt']
 print(paste0('The most promising place to open the caravan business is ', city, '.'))
-# "The most promising place to open the caravan business is Steinfurt."
+# "The most promising place to open the caravan business is Dortmund."
