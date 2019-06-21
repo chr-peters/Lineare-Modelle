883901091f978454a834116fc80919961ebe4fe1
cxan <c.p.xanten@gmail.com>
2019-06-21 17:16:20 +0200

    added exercise 33

--- 12/peters-12.R
+++ 12/peters-12.R
@@ -55,7 +55,7 @@ print(intervals_scheffe)
 
 # draw the confidence ellipse using lm
 model_cobbdouglas <- lm(log(y) ~ log(xL) + log(xK), data = cobbdouglas)
-confidenceEllipse(model_cobbdouglas, xlab = 'beta1', ylab = 'beta2', grid = FALSE)
+confidenceEllipse(model_cobbdouglas, xlab = 'beta1', ylab = 'beta2', main = 'Confidence Ellipse and Intervals', grid = FALSE)
 
 # draw the bonferroni intervals (red)
 abline(v = intervals_bonferroni['beta1', 'lower'], col = 'red', lwd = 2)
@@ -115,3 +115,89 @@ confidenceEllipse(model, main = 'Confidence Ellipse for a1 and a2 coefficients')
 
 # No. 33)
 # =======
+
+cuckoo <- read.table('kuckuck.txt', header = TRUE)
+
+# a)
+
+# Fit a balanced ANOVA model:
+# y = mu + a_i + eij
+# y := length of egg
+# mu := mean of egg length
+# a_i := influence of i'th species on egg length
+# eij := Error
+# Side condition: sum(n_i * a_i) = 0
+
+# create the design matrix and the response vector
+n_WP <- sum(!is.na(cuckoo$WP))
+n_BP <- sum(!is.na(cuckoo$BP))
+n_RK <- sum(!is.na(cuckoo$RK))
+n_ZK <- sum(!is.na(cuckoo$ZK))
+n <- n_WP + n_BP + n_RK + n_ZK
+X_cuckoo <- cbind(1, c(rep(1, n_WP), numeric(n - n_WP)), c(numeric(n_WP), rep(1, n_BP), numeric(n_RK + n_ZK)),
+           c(numeric(n_WP + n_BP), rep(1, n_RK), numeric(n_ZK)), c(numeric(n - n_ZK), rep(1, n_ZK)))
+colnames(X_cuckoo) <- c('mu', 'a_WP', 'a_BP', 'a_RK', 'a_ZK')           
+y_cuckoo <- c(cuckoo$WP[!is.na(cuckoo$WP)], cuckoo$BP[!is.na(cuckoo$BP)], cuckoo$RK[!is.na(cuckoo$RK)], cuckoo$ZK[!is.na(cuckoo$ZK)])
+
+# Find the coefficients for a balanced model
+B <- matrix(c(0, n_WP, n_BP, n_RK, n_ZK), nrow = 1)
+X_B_cuckoo <- X_cuckoo %*% (diag(5) - ginv(B) %*% B)
+X_B_inv_cuckoo <- ginv(X_B_cuckoo)
+beta_cuckoo <- X_B_inv_cuckoo %*% y_cuckoo
+rownames(beta_cuckoo) <- c('mu', 'a_WP', 'a_BP', 'a_RK', 'a_ZK')
+cat('\n')
+print('Model coefficients:')
+print(beta_cuckoo)
+
+# b)
+
+# Use an F-Test to test if the effects of bird species are different.
+# Precondition: The parametric function that is used is LE-estimable (which is the case here).
+
+# Hypothesis: C * beta = 0
+C_cuckoo <- matrix(c(0, 1, -1, 0, 0, 0, 0, 1, -1, 0, 0, 0, 0, 1, -1), nrow = 3, byrow = TRUE)
+
+# compute the test statistic
+residuals_cuckoo <- y_cuckoo - X_cuckoo %*% beta_cuckoo
+ne_cuckoo <- length(residuals_cuckoo) - 5
+var_cuckoo <- sum(residuals_cuckoo**2) / ne_cuckoo
+# rank(CB) = 3
+teststat <- t(C_cuckoo %*% beta_cuckoo) %*% ginv(C_cuckoo %*% X_B_inv_cuckoo %*% t(X_B_inv_cuckoo) %*% t(C_cuckoo)) %*%
+  (C_cuckoo %*% beta_cuckoo) / (3 * var_cuckoo)
+# 13.32955
+
+# compute the critical value
+critical <- qf(0.9, 3, ne_cuckoo)
+# 2.14859
+
+# We can see that the test statistic is greater than the critical value, which means
+# that there is significant evidence that the lengths of the cuckoo eggs differ depending
+# on the species of bird which breeds them.
+# We reject H0 at the 10% niveau.
+
+# c)
+
+# iterate over all possible combinations of bird species and perform a bonferroni
+# test for each
+invisible(sapply(combn(c('a_WP', 'a_BP', 'a_RK', 'a_ZK'), 2, simplify = FALSE), function(pair) {
+  cat('\nTest ', pair[1], ' vs. ', pair[2], ':\n')
+  
+  # create hypothesis matrix
+  pos_1 <- match(pair[1], rownames(beta_cuckoo))
+  pos_2 <- match(pair[2], rownames(beta_cuckoo))
+  C <- matrix(0, ncol = 5)
+  C[1, pos_1] = 1
+  C[1, pos_2] = -1
+  
+  # calculate test statistic
+  teststat <- abs(C %*% beta_cuckoo)
+
+  # estimate the standard deviation
+  sd_est <- sqrt(var_cuckoo * C %*% X_B_inv_cuckoo %*% t(X_B_inv_cuckoo) %*% t(C))
+
+  # estimate the critical value:
+  critical <- qt(1 - 0.1 / (2 * 6), ne_cuckoo) * sd_est
+  
+  cat('Value of teststat: ', teststat, ', Critical value: ', critical)
+  cat('\nResult: ', ifelse(teststat > critical, 'Rejection (H1)', 'No rejection (H0)'), '\n')
+}))
